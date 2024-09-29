7a6cd2767ef46565e6e6468e4e087a05de64f941
Ameera <ameerathiwanka@gmail.com>
2024-09-29 16:15:31 +0530

    validation is done

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask, request, jsonify
-from data_processing import process_tasks;
 from validation import validate_data;
-from authorization import require_api_token;
-from pagination import paginate_tasks;
+
 
 app = Flask(__name__)
 
@@ -14,9 +12,8 @@ def submit():
     if not is_valid:
         return jsonify({"errors": errors}), 400
     
-    # Process the valid data here
-    summary = process_tasks(data['tasks'])
-    return jsonify({"message": "Data submitted successfully", "summary": summary}), 200
+
+    return jsonify({"message": "Data is Valid"}), 200
 
 
 if __name__ == '__main__':
